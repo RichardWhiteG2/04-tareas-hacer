2e7496c9fade4f35dfccd6287742706df003a7dd
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-08-03 20:11:33 -0600

    Menu Interactivo

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-require('colors');
 
-const { mostrarMenu } = require('./helpers/mensajes');
+require('colors');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 console.clear();
 
 const main = async() => {
-
-    console.log('Hola Mundo');
-
-
-    mostrarMenu();
-
-
+    let opt= '';
+    
+    do{
+        // opt = await mostrarMenu();
+        opt = await inquirerMenu();
+        console.log({opt});
+        await pausa();
+    }while( opt !=='0' );
 }
 
 main();
